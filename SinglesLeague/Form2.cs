cc6341f0e679f7d931f71021d1a208328e3b994a
JoeG05 <jgualdar@ramapo.edu>
2017-05-19 15:33:43 -0400

    started lowerstats submission

--- SinglesLeague/Form2.cs
+++ SinglesLeague/Form2.cs
@@ -19,6 +19,33 @@ namespace SinglesLeague
 
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
+            sql s = new sql();
+
+            TextBox[] scored = { };
+            int scoredTotal = 0;
+
+            TextBox[] thrown = { };
+            int thrownTotal = 0;
+
+            TextBox[] allstars = { };
+            int allstarsTotal = 0;
+
+            int wins = 0;
+            int losses = 0;
+            int week = Convert.ToInt32(textBoxWeek.Text);
+            string name = comboBox1.Text;
+
+            for (int i = 0; i < 7; i++)
+            {
+                scoredTotal += Convert.ToInt32(scored[i]);
+                thrownTotal += Convert.ToInt32(thrown[i]);
+                allstarsTotal += Convert.ToInt32(allstars[i]);
+
+                if (Convert.ToInt32(scored[i]) == 0)
+                    wins++;
+
+            }
+
             this.Close();
         }
     }
